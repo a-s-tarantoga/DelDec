e0b615b4d1f053d10aa176e31db81b2f1159a8f1
Martin Hünniger <a_s_tarantoga@yahoo.de>
2014-07-31 11:23:18 +0200

    Done Licensing to GPL
    TODO: implement unit tests
    TODO: augment CMakeLists.txt to work on other platforms

--- helper.h
+++ helper.h
@@ -1,5 +1,21 @@
 /*
  * (c) 30.07.2014 Martin Hünniger
+ *
+ *   This file is part of DelDec.
+ *
+ *   DelDec is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   DelDec is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 #ifndef HELPER_H
 #define HELPER_H
